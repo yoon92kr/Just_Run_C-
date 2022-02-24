199f45f548cd98e416730736721beb66926b02d4
yoon92kr <yoon92_kr@naver.com>
2022-02-24 12:40:19 +0900

    2022.02.24 Write
    ▶ 콘솔에 출력을 도와주는 내장 메소드, java의 printf 메소드와 유사하다.

--- Code001.cs
+++ Code001.cs
@@ -1,9 +1,12 @@
-﻿List<String> strList = new List<String>();
+static void Code001()
+{
+    System.Console.Write("Hello");
+    System.Console.Write("World");
+    // 개행 없이 문자열을 콘솔에 출력한다.
 
-strList.Add("hello");
-strList.Add("world");
-
-
-foreach(String str in strList) {
-    Console.WriteLine(str);
+    System.Console.WriteLine("Hello");
+    System.Console.WriteLine("World");
+    // 콘솔에 문자열 출력 후 개행처리를 도와준다.
 }
+
+Code001();
